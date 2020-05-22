c525d2b2cca0a2aad9cbe9c55f93eba7ced27fef
Amine BENBAKHTA <gm_benbakhta@esi.dz>
2020-05-23 00:27:21 +0100

    Creating databse configuration and connexion test

---
+++ config/index.js
@@ -0,0 +1,9 @@
+const configValues = require('./config')
+
+module.exports = {
+
+    getDbConnectionString: function(){
+        return 'mongodb://' + configValues.username + ':' + configValues.password + '@localhost:27017/db-ambre'
+    }
+    
+}
